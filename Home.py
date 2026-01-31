408f36523ca734f8f40f35250458f92251c25f9d
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-01-31 22:35:58 +0100

    Update title for Venice CO2 Phase 3

--- Home.py
+++ Home.py
@@ -1,8 +1,5 @@
 import streamlit as st
 st.set_page_config(page_title="Venice Phase 3", layout="wide")
-st.title("Venice CO2")
+st.title("Venice CO2 Phase 3")
 st.write("Use the sidebar to open: CO₂ Map and CO₂ Boxplots.")
 st.write("copyright content Swarnali Mollick")
-
-
-
