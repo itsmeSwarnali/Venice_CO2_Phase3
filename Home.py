e1786f4a33f38cef77146e16db0803ee1c65876a
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-01-31 22:10:00 +0100

    Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.set_page_config(page_title="Venice Phase 3", layout="wide")
-st.title("V:contentReference[oaicite:7]{index=7}esentations")
+st.title("Venice CO2 [oaicite:7]{index=7}")
 st.write("Use the sidebar to open: CO₂ Map and CO₂ Boxplots.")
+
