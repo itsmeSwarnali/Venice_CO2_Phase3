eb2315707ac56f4f788aa1ff7db52352525b6663
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-02-16 15:59:17 +0100

    Add copyright notice to Home.py
    Added copyright notice to the main page.

--- Venice_Phase3/Home.py
+++ Venice_Phase3/Home.py
@@ -2,5 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="Venice Phase 3", layout="wide")
 st.title("Venice CO2 Data Exploration - Phase 3")
 st.write("Use the sidebar to open: CO₂ Map and CO₂ Boxplots.")
+st.write("© Swarnali Mollick, 908523")
+
 
 
