57bd63dc95cc069074076436b09d5ac3710e99cb
Swarnali Mollick <76970926+itsmeSwarnali@users.noreply.github.com>
2026-02-16 16:00:25 +0100

    Update sidebar instructions in Home.py

--- Venice_Phase3/Home.py
+++ Venice_Phase3/Home.py
@@ -1,8 +1,9 @@
 import streamlit as st
 st.set_page_config(page_title="Venice Phase 3", layout="wide")
 st.title("Venice CO2 Data Exploration - Phase 3")
-st.write("Use the sidebar to open: CO₂ Map and CO₂ Boxplots.")
+st.write("Use the sidebar to open: CO₂ Boxplots, CO₂ Map, CO₂ Violin Plot and Others")
 st.write("© Swarnali Mollick, 908523")
+
 
 
 
